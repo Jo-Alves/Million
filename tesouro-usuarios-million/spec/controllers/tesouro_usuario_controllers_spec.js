ac937eec2c0af6fcff3397ac334c1486e059a966
Jo-Alves <49097495+Jo-Alves@users.noreply.github.com>
2020-10-15 11:38:30 -0300

    [API] tesouro usuario - inclementado o método get, put e delete

--- tesouro-usuarios-million/spec/controllers/tesouro_usuario_controllers_spec.js
+++ tesouro-usuarios-million/spec/controllers/tesouro_usuario_controllers_spec.js
@@ -20,4 +20,75 @@ describe("TDD Tesouro Usuário Controller", () => {
             done()
         })
     })
+    
+    describe("GET/tesouros-usuario.json", () => {
+        it("Deve retornar o status de 200 e o código do tesouro", async done => {
+            body = {
+                codigoTesouro: "VELMA",
+                codigoUsuario: "sf828",
+                valorInvestido: 50,
+                dataInvestimento: new Date()
+            }
+            await TesouroUsuario.create(body)
+            const response = await axios.get(`${host}/tesouros-usuario.json`, body);
+            expect(response.data.codigoTesouro).toBe("VELMA")
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+    describe("GET/tesouros-usuario/:id.json", () => {
+        it("Deve retornar o status de 201", async done => {
+            body = {
+                codigoTesouro: "FRINSTONE",
+                codigoUsuario: "sf828",
+                valorInvestido: 50,
+                dataInvestimento: new Date()
+            }
+            const tesouroUsuario = await TesouroUsuario.create(body)
+            const response = await axios.get(`${host}/tesouros-usuario.json/${tesouroUsuario._id}`, body);
+            expect(response.data.codigoTesouro).toBe("VELMA")
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+    
+    describe("PUT/tesouros-usuario/:id.json", () => {
+        it("Deve retornar o status de 204", async done => {
+            const body = {
+                codigoTesouro: "BARNEEEEEEEY",
+                codigoUsuario: "sf828",
+                valorInvestido: 50,
+                dataInvestimento: new Date()
+            }
+
+            const bodyUpdate = {
+                codigoTesouro: "BARNEY",
+                codigoUsuario: "sf828",
+                valorInvestido: 50,
+                dataInvestimento: new Date()
+            }
+            const tesouroUsuario = await TesouroUsuario.create(body)
+            const response = await axios.put(`${host}/tesouros-usuario.json/${tesouroUsuario._id}`, bodyUpdate);
+            expect(response.data.codigoTesouro).toBe("BARNEY")
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+    
+    describe("DELETE/tesouros-usuario/:id.json", () => {
+        it("Deve retornar o status de 201", async done => {
+            body = {
+                codigoTesouro: "FRINSTONE",
+                codigoUsuario: "sf828",
+                valorInvestido: 50,
+                dataInvestimento: new Date()
+            }
+            const tesouroUsuario = await TesouroUsuario.create(body)
+            const response = await axios.get(`${host}/tesouros-usuario.json/${tesouroUsuario._id}`);
+            expect(response.data).toBe("")
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
 })
