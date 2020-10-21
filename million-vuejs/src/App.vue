f02019bcd26896361080b39344b5fa7b0f0f8b83
Jô Developer <joalves3carvalho@gmail.com>
2020-10-20 23:10:41 -0200

    million-vuejs

---
+++ million-vuejs/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <AppHeader></AppHeader>
+    <router-view></router-view>
+    <AppFooter></AppFooter>
+  </div>
+</template>
+
+<script>
+import AppFooter from "./components/app-footer.vue"
+import AppHeader from "./components/app-header.vue"
+import "./assets/css/main.css"
+
+export default {
+  name: 'App',
+  components: {
+    AppFooter,
+    AppHeader,
+  }
+}
+</script>
